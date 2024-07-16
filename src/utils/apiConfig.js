538bfbb776c9689d6a5904a95389dadfd1bfda43
Haeun <hekoh99@naver.com>
2024-07-16 12:46:14 +0900

    deploy

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,4 +1,4 @@
-const port = 8080;
-const backendHost = `http://localhost:${port}`
+const port = 5000;
+const backendHost = `http://ec2-3-36-121-168.ap-northeast-2.compute.amazonaws.com:${port}`
 
 export const API_BASE_URL = `${backendHost}`;
